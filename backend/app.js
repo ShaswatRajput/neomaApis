f1606e311991f30488ad463938c2710fd756013b
Shaswat <shaswatrajput7@gmail.com>
2023-12-23 11:19:40 +0530

    demo video added to the default page

--- backend/app.js
+++ backend/app.js
@@ -27,7 +27,18 @@ app.get("/", async (req, res) => {
             await Location.insertMany(data.data);
         }
 
-        res.status(200).send(`<h1>Welcome to the Neoma Real Estate Marketplace</h1>`)
+        res.status(200).send(`<center>
+        <h1>Welcome to the Neoma Real Estate Apis</h1>
+        <ul>
+        <li> GET /properties    ---Retrieve all properties.</li>
+        <li>GET /properties/:id    ---Retrieve a property by ID.</li>
+        <li>POST /properties    ---Add a new property.</li>
+        <li>PUT /properties/:id    ---Update a property by ID.</li>
+        <li>DELETE /properties/:id    ---Delete a property by ID.</li>
+        <li>GET /properties/search    ---Search and filter properties based on location, price range, and property type.</li>
+        </ul>
+        <iframe width="1280" height="720" src="https://www.youtube.com/embed/O-tmHDm45Ao?si=qM4_2xaFAsD7J8h3" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </center>`)
     } catch (error) {
         console.log(error)
         res.status(500).json({
